95596821e263479ed0884b514a5889a2b1de79a4
Livia <livinha.guimaraes.013@gmail.com>
2022-01-14 11:15:05 -0300

    Show score

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Navbar() {
@@ -6,7 +7,9 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <Link to="/">
+                        <h1>DSMovie</h1>
+                    </Link>
                     <a href="https://github.com/4L1C3-R4BB1T" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
